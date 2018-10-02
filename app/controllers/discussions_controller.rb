38a6436dfccab8d5b6a15d39fcc32649b5e1967b
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-02 13:25:25 +0200

    discussions ok 90%

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,13 +1,35 @@
 class DiscussionsController < ApplicationController
+  def new
+    @event= Event.find(params[:event_id])
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+    event = Event.find(params[:event_id])
+    # we need `event_id` to asssociate the discussion with corresponding event
+    @discussion.event = event
+    @discussion.user = current_user
+    @discussion.save
+    redirect_to event_path(event)
+    flash[:notice] = "Thanks for your message!"
+  end
+
+
   def index
   end
 
-  def new
-  end
 
   def edit
   end
 
   def delete
   end
+
+    private
+
+  def discussion_params
+    params.require(:discussion).permit(:message_event, :user_id, :event_id)
+  end
+
 end
