d02a260bf9e0fe3e5b66d4963dad4214f20cee08
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-02 16:15:26 +0200

    join event

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,7 +1,21 @@
 class JoinsController < ApplicationController
   def new
+    @event= Event.find(params[:event_id])
+    @join = Join.new
   end
 
   def create
+    @join = Join.new()
+    event = Event.find(params[:event_id])
+    # we need `event_id` to asssociate the discussion with corresponding event
+    @join.event = event
+    @join.user = current_user
+    @join.save
+    redirect_to event_path(event)
+    flash[:notice] = "See you at the event!"
   end
+    private
+
+
+
 end
