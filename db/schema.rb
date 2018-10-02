2efd4e4257546ec52a4fb825e0e845ccbdc10e18
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-02 19:52:52 +0200

    push heroku new phone as string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_174725) do
+ActiveRecord::Schema.define(version: 2018_10_02_175134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_174725) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photoprofile"
+    t.string "phone"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
