f4cf538f2f1770153d46f3dcc5c5906008df4209
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-03 18:47:39 +0200

    try on heroku

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,8 +2,8 @@ function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var eventLocation = document.getElementById('event_location');
 
-    if (flatAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
+    if (eventAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(eventAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(eventLocation, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,2 @@
 import "bootstrap";
-
+import autocomplete from "../components/autocomplete"
