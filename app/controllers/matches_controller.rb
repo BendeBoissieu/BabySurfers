875f937214ef1667f903f038edbc5e390c65d5d6
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-07 16:26:19 +0200

    with flash to pass a variable from new to create

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,38 +2,27 @@ class MatchesController < ApplicationController
 
   before_action :redirect_dontlike, only: [:create]
 
+
   def new
     @match = Match.new
-    @users = User.all
     #Read the json file
-    file = open("./public/event.json")
-    json = file.read
-    @parsed = JSON.parse(json)
-
-    @sampleUser = @parsed.sample(1)
-    while @sampleUser[0][:id] == current_user.id
-    @sampleUser = @parsed.sample(1)
-    end
+    sample();
+    flash['@sampleUser'] = @sampleUser
 
 
 
   end
 
   def create
-
-    @match = Match.new(match_params)
-    user = User.find(params[:match_id])
+    @match = Match.new()
+    @match.user = current_user
+    @sampleUser = flash['@sampleUser']
     # we need `event_id` to asssociate the discussion with corresponding event
-    #if @match_user_one_id.present?
-    #  @match.user_two_id = current_user.id
-    #    if @match_user_one_id.present? || @match_user_two_id.present?
-    #      flash[:notice] = "you have a match!"
-    #    else
-    #      flash[:notice] = "you like"
-    #    end
-    #else
-    #    @match.user_one_id = current_user.id
-    #end
+    @match.user_one_id = current_user.id
+    @match.user_two_id = @sampleUser[0]["id"]
+
+    #@match_user_two_id = @sampleUser[0]["id"]
+
     if @match.save
       redirect_to root_path
       flash[:notice] = "you like"
@@ -67,10 +56,8 @@ class MatchesController < ApplicationController
     end
   end
 
-    private
-
   def match_params
-    params.require(:match).permit(:match_id, :user_one_id, :user_two_id)
+    params.require(:match).permit(:user_id, :user_one_id, :user_two_id)
   end
 
   def redirect_dontlike
@@ -80,6 +67,22 @@ class MatchesController < ApplicationController
     flash[:notice] = "you did not like"
     end
   end
+
+
+  def sample
+    file = open("./public/event.json")
+    json = file.read
+    @parsed = JSON.parse(json)
+
+    @sampleUser = @parsed.sample(1)
+    while @sampleUser[0][:id] == current_user.id
+    @sampleUser = @parsed.sample(1)
+    end
+    return @sampleUser
+  end
+
+
+
 
 
 end
