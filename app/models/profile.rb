76d32227616f1c253fdf9721fff043430b686ffc
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-08 20:56:01 +0200

    collections on profile show

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,8 @@ class Profile < ApplicationRecord
   mount_uploader :photoprofile, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  SURF_LEVEL = ["very baby", "baby", "super baby", "papy", "super papy", "papy pro"]
+  I_LIKE = ["beers", "BBQ", "coffee", "blabla", "volley-ball", "chill", "other ideas?"]
+  BOARD_TYPE = ["no board", "soft board", "hard board"]
 end
