5871ca89572e99e08145998af73aa6a19c04aac8
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-09 20:54:37 +0200

    add dependence to destroy the childs models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :discussions
-  has_many :joins
+  has_many :discussions, :dependent => :delete_all
+  has_many :joins, :dependent => :delete_all
   geocoded_by :location
   after_validation :geocode, if: :location_changed?
 end
