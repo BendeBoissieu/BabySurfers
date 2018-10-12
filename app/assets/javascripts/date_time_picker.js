e5776aaabfc87680fcbd1a310eb91712ad481a8a
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-12 14:47:20 +0200

    punctuation js unglifer

--- app/assets/javascripts/date_time_picker.js
+++ app/assets/javascripts/date_time_picker.js
@@ -1,8 +1,8 @@
 $( document ).on('turbolinks:load', function() {
-  $('#datetimepicker').datetimepicker((function){
+  $('#datetimepicker').datetimepicker(function){
     console.log('test');
     locale: 'fr',
     sideBySide: true,
     format: "DD-MM-YYYY h:mm A"
-  });
+  };
 });
