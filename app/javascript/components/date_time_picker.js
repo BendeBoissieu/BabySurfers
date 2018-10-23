fb9136534cfd09f96937d43755f100b592407d3a
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-23 18:31:30 +0200

    swiper ok

---
+++ app/javascript/components/date_time_picker.js
@@ -0,0 +1,14 @@
+function dateTimepicker() {
+$( document ).on('turbolinks:load', function() {
+  $('#datetimepicker').datetimepicker({
+    sideBySide: true,
+    format: "DD-MM-YYYY h:mm A"
+  });
+});
+
+
+}
+
+dateTimepicker();
+export { dateTimepicker };
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
+import "swiper";
+import "moment";
 import $ from "jquery";
 import autocomplete from "../components/autocomplete";
 import superSwiper from "../components/swiper";
-
-
+import dateTimepicker from "../components/date_time_picker";
