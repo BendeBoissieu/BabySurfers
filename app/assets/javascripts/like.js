a06973e953f039b2f649408c4387a2d3de6a8fc9
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-10-28 20:42:03 +0100

    style

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,18 +1,14 @@
 $('document').ready(function (){
-  var myVar = document.getElementById("aloha").addEventListener("click", likeSurfer);
-  function likeSurfer(){
-
+  //var myVar = document.getElementById("aloha").addEventListener("click", likeSurfer);
+  //function likeSurfer(){
+    //console.log('I like');
    //console.log(mySwiper.activeIndex);
    // FIND THE USER ID OF THE ACTIVE SWIPER CARD
-  // var activenb = mySwiper.activeIndex;
-  // var idUser = mySwiper.slides[activenb].firstChild.nextElementSibling.innerText
-   //  console.log(idUser);
+   //var activenb = mySwiper.activeIndex;
+   //var idUser = mySwiper.slides[activenb].firstChild.nextElementSibling.innerText
+     //console.log(idUser);
    //document.getElementById("demo").innerHTML = idUser;
-
-
-
-
-  }
+  //}
 
 
   var mySwiper = new Swiper('.swiper-container', {
