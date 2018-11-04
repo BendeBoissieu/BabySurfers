f06a91063076ad68d6ca16e35314eeaf505af5ff
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-11-04 19:43:00 +0100

    conversations seems to be good

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,6 +10,17 @@ class ConversationsController < ApplicationController
     # conversations = policy_scope(Conversation)
     @user = current_user
     conversations = Conversation.all
+    query = <<-SQL
+      SELECT * FROM conversations
+      WHERE id IN
+      (
+        SELECT user_two_id FROM matches WHERE user_one_id = :current_user_id AND mutual = TRUE
+        UNION
+        SELECT user_one_id FROM matches WHERE user_two_id = :current_user_id AND mutual = TRUE
+      )
+      SQL
+      conversations = Conversation.find_by_sql([ query, { current_user_id: current_user.id }])
+
    if conversations == []
 
      redirect_to profiles_path, alert: 'You have to match with someone to start a conversation!'
