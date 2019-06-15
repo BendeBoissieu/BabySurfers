279644d8d7bc01fc53c40c835f47a77e9967d7d1
benjamin de boissieu <bendeb@hotmail.com>
2019-06-15 20:13:53 +0200

    new model rental

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_153421) do
+ActiveRecord::Schema.define(version: 2019_06_15_181137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,21 @@ ActiveRecord::Schema.define(version: 2018_10_30_153421) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
+    t.string "phone"
+    t.integer "price"
+    t.string "picture"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -158,4 +173,5 @@ ActiveRecord::Schema.define(version: 2018_10_30_153421) do
   add_foreign_key "profile_likecategories", "likecategories"
   add_foreign_key "profile_likecategories", "profiles"
   add_foreign_key "profiles", "users"
+  add_foreign_key "rentals", "users"
 end
