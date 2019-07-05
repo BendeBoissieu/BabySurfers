5211b86d3434836e6feb3d733b27d344946f5078
benjamin de boissieu <bendeb@hotmail.com>
2019-07-05 14:08:51 +0200

    geocoding rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,6 @@
 class Rental < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PhotoUploader
+  geocoded_by :localisation
+  after_validation :geocode, if: :will_save_change_to_address?
 end
