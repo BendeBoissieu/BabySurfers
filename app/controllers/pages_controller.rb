26052d38189eca13c2b31fa96c6467b9314f3d32
benjamin de boissieu <bendeb@hotmail.com>
2019-08-21 19:10:13 +0200

    condition home if user is login or not to display presentation find a babysurfers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
    skip_before_action :authenticate_user!, only: [:home,:about, :presentation_find]
   def home
+    @user = current_user
   end
 end
 
