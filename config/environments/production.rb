bf0c37081ca4e80f77575db9d146fc777e2c9390
benjamin de boissieu <bendeb@hotmail.com>
2019-09-16 23:58:01 +0200

    try to solve production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -96,7 +96,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.action_mailer.delivery_method = :smtp
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://babysurfers.herokuapp.com'}
-
+  host = 'babysurfers.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['GMAIL_ADDRESS'],
+    :password       => ENV['GMAIL_APP_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
 end
